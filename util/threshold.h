8f8e0964810c81eab8f1f5644355d5915f6422db
kystyn <konstantindamaskinskiy@gmail.com>
2019-10-24 02:07:57 +0300

    corrected threshold

--- util/threshold.h
+++ util/threshold.h
@@ -18,51 +18,47 @@ public:
         const int matrixSize = 3;
         std::array<int, matrixSize * matrixSize> indices;
 
-        // pixel - center pixel
-        auto applyMedian =
-                [&imgData, cpp, &indices, matrixSize]( int pixelX, int pixelY, int channel, stbi_uc *saved ) -> void
-        {
-            int
-                xStart = std::max(0, pixelX - matrixSize / 2),
-                yStart = std::max(0, pixelY - matrixSize / 2),
-                xEnd = std::min(imgData.w - 1, pixelX + matrixSize / 2),
-                yEnd = std::min(imgData.h - 1, pixelY + matrixSize / 2);
-
-            int i = 0;
-            for (int y = yStart; y <= yEnd; y++)
-                for (int x = xStart; x <= xEnd; x++)
-                    indices[i++] = (y * imgData.w + x) * cpp + channel;
-
-            std::sort(indices.begin(),
-                      indices.begin() + (yEnd - yStart + 1) * (xEnd - xStart + 1),
-                        [saved]( int idx1, int idx2 ) -> bool
-                        {
-                            return saved[idx1] < saved[idx2];
-                        });
-
-            for (int i = 0; i < (yEnd - yStart + 1) * (xEnd - xStart + 1) / 2; i++)
-                imgData.pixels[indices[i]] = 0;
-        };
-
         if (cpp == 4 || cpp == 3) {
             int y = ar.top == 0 ? 0 : imgData.h / ar.top;
             int x = ar.left == 0 ? 0 : imgData.w / ar.left;
-            stbi_uc *saved = new stbi_uc[(imgData.h / ar.bottom - y) * (imgData.w / ar.right - x) * cpp];
-
+            stbi_uc *saved = new stbi_uc[imgData.h * imgData.w * cpp];
             if (saved == nullptr)
                 throw "No memory";
 
-            int i = 0;
-            for (i = 0; i < imgData.h / ar.bottom - y; i++)
-                memcpy(saved + (i * imgData.w + x) * cpp,
-                       imgData.pixels + ((y + i) * imgData.w + x) * cpp, (imgData.w / ar.right - x) * cpp);
+            memcpy(saved, imgData.pixels, imgData.w * imgData.h * cpp);
+
+            // pixel - center pixel
+            auto applyMedian =
+                    [&imgData, cpp, &indices, matrixSize, saved]( int pixelX, int pixelY, int channel ) -> void
+            {
+                int
+                    xStart = std::max(0, pixelX - matrixSize / 2),
+                    yStart = std::max(0, pixelY - matrixSize / 2),
+                    xEnd = std::min(imgData.w - 1, pixelX + matrixSize / 2),
+                    yEnd = std::min(imgData.h - 1, pixelY + matrixSize / 2);
+
+                int i = 0;
+                for (int y = yStart; y <= yEnd; y++)
+                    for (int x = xStart; x <= xEnd; x++)
+                        indices[i++] = (y * imgData.w + x) * cpp + channel;
+
+                std::sort(indices.begin(),
+                          indices.begin() + (yEnd - yStart + 1) * (xEnd - xStart + 1),
+                            [saved]( int idx1, int idx2 ) -> bool
+                            {
+                                return saved[idx1] < saved[idx2];
+                            });
+
+                for (int i = 0; i < (yEnd - yStart + 1) * (xEnd - xStart + 1) / 2; i++)
+                    imgData.pixels[indices[i]] = 0;
+            };
 
 
             for (; y < imgData.h / ar.bottom; y++) {
                 x = ar.left == 0 ? 0 : imgData.w / ar.left;
                 for (; x < imgData.w / ar.right; x++)
                     for (int c = 0; c < 3; c++)
-                        applyMedian(x, y, c, saved);
+                        applyMedian(x, y, c);
             }
 
             delete []saved;
