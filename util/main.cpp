222d1dc3fe95e99e521c60ea596334b608da632e
kystyn <konstantindamaskinskiy@gmail.com>
2019-10-24 22:23:57 +0300

    normal argc returned :)

--- util/main.cpp
+++ util/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include "png_toolkit.h"
 #include "red_filter.h"
 #include "threshold.h"
@@ -24,20 +25,21 @@ void brightness( image_data const &imgData )
 
 int main( int argc, char *argv[] )
 {
-    // toolkit config base_pic_name student_pic_name
+    // toolkit filter_name ref_pic_name student_pic_name limitPix limitMSE
+    // top left bottom right
+    // OR
+    // toolkit config base_pic_name ref_pic_name stud_tool stud_pic_name #it means - generate reference my and student image
+    // config format: <filter name> top left bottom right
     // toolkit near test images!
     try
     {
-        if (argc != 4)
+        if (argc != 10 && argc != 6)
             throw "Not enough arguments";
 
-        png_toolkit studTool;
+        png_toolkit testTool;
+        testTool.load(argv[2]);
 
-        if (!studTool.load(argv[2]))
-        {
-            std::cout << "Bad file name\n";
-            return 0;
-        }
+        // fill all filters
         filter::red r("Red");
         filter::threshold t("Threshold", brightness);
         filter::convolution<3> blur("Blur",
@@ -53,36 +55,68 @@ int main( int argc, char *argv[] )
                 {-1.0, -1.0, -1.0}
             }, brightness);
 
-
-        std::ifstream ifs(argv[1]);
-        if (!ifs)
-            throw  "Bad file name";
-
-        studTool.load(argv[2]);
-        while (ifs)
+        if (argc == 6)
         {
-            std::string filterName;
-            filter::base::area ar;
-            ifs >> filterName >> ar;
+            std::ifstream ifs(argv[1]);
+            if (!ifs)
+                throw "Bad file name";
 
-            if (filterName.empty())
-                break;
+            testTool.load(argv[2]);
+            while (ifs)
+            {
+                std::string filterName;
+                filter::base::area ar;
+                ifs >> filterName >> ar;
 
-            auto f = filter::base::getFilters().find(filterName);
-            if (f != filter::base::getFilters().end())
-                studTool.applyFilter(*(f->second), ar);
-            else
-                throw "Bad filter";
+                if (filterName.empty())
+                    break;
+
+                auto f = filter::base::getFilters().find(filterName);
+                if (f != filter::base::getFilters().end())
+                    testTool.applyFilter(*(f->second), ar);
+                else
+                    throw "Bad filter";
+            }
+            testTool.save(argv[3]);
+            std::string
+                    config = std::filesystem::absolute(argv[1]),
+                    base = std::filesystem::absolute(argv[2]),
+                    ref = std::filesystem::absolute(argv[5]);
+            system(("./" + std::string(argv[4]) + ' ' + config + ' ' + base + ' ' + ref).c_str());
+            return 0;
         }
 
-        if (!studTool.save(argv[3]))
-            throw "Could not save picture";
+        testTool.load(argv[2]);
+        png_toolkit studTool;
+        studTool.load(argv[3]);
 
-        std::cout << "OK\n";
+        filter::base::area ar;
+        ar.top = std::stoi(argv[6]);
+        ar.left = std::stoi(argv[7]);
+        ar.bottom = std::stoi(argv[8]);
+        ar.right = std::stoi(argv[9]);
+
+        png_toolkit::Error err;
+        int diffPix;
+        auto mse = testTool.mseDeviation(studTool, err, diffPix, ar);
+        if (err == png_toolkit::Error::Ok)
+        {
+            if (diffPix < std::stoi(argv[4]))
+                std::cout << "OK ";
+            else
+                std::cout << "BAD: Too many bad pixels: " << diffPix << " ";
+
+            if (mse < std::stof(argv[5]))
+                std::cout << "OK";
+            else
+                std::cout << "BAD: Too big MSE: " << mse;
+        }
+        else
+            throw "Bad picture size/format";
     }
     catch (const char *str)
     {
-        std::cout << "Error: " << str << std::endl;
+        std::cout << "BAD: Error: " << str << std::endl;
         return 1;
     }
 
