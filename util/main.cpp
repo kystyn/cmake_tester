1a0dbe295a0baaaf6c9d3d17e1765511031b33c2
kystyn <konstantindamaskinskiy@gmail.com>
2019-11-11 22:38:34 +0300

    concept changed

--- util/main.cpp
+++ util/main.cpp
@@ -28,15 +28,14 @@ void brightness( image_data const &imgData )
 
 int main( int argc, char *argv[] )
 {
-    // toolkit filter_name ref_pic_name student_pic_name limitPix limitMSE
-    // top left bottom right
+    // toolkit config filter_name ref_pic_name student_pic_name limitPix limitMSE
     // OR
     // toolkit config base_pic_name ref_pic_name stud_tool stud_pic_name #it means - generate reference my and student image
     // config format: <filter name> top left bottom right
     // toolkit near test images!
     try
     {
-        if (argc != 10 && argc != 6)
+        if (argc != 7 && argc != 6)
             throw "Not enough arguments";
 
         png_toolkit testTool;
@@ -58,12 +57,12 @@ int main( int argc, char *argv[] )
                 {-1.0, -1.0, -1.0}
             }, brightness);
 
+        std::ifstream ifs(argv[1]);
+        if (!ifs)
+            throw "Bad config name";
+
         if (argc == 6)
         {
-            std::ifstream ifs(argv[1]);
-            if (!ifs)
-                throw "Bad file name";
-
             testTool.load(argv[2]);
             while (ifs)
             {
@@ -95,10 +94,17 @@ int main( int argc, char *argv[] )
         studTool.load(argv[3]);
 
         filter::base::area ar;
-        ar.top = std::stoi(argv[6]);
-        ar.left = std::stoi(argv[7]);
-        ar.bottom = std::stoi(argv[8]);
-        ar.right = std::stoi(argv[9]);
+        std::string filterName;
+
+        while (ifs)
+        {
+            ifs >> filterName;
+            if (filterName == argv[2])
+            {
+                ifs >> ar;
+                break;
+            }
+        }
 
         png_toolkit::Error err;
         int diffPix;
